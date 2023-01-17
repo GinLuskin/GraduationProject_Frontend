f91534edf14572c71577852d55b3776fa5223614
Regina Luskin <reginash12@gmail.com>
2023-01-17 21:36:17 +0200

    authorization and authentication complere

--- GraduationProject/src/App.js
+++ GraduationProject/src/App.js
@@ -1,32 +1,22 @@
-import React, { useState, useCallback } from "react";
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
-  Switch,
-} from "react-router-dom";
+  Switch
+} from 'react-router-dom';
 
-import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
-import UserPlaces from "./places/pages/UserPlaces";
-import UpdatePlace from "./places/pages/UpdatePlace";
-import Auth from "./user/pages/Auth";
-import MainNavigation from "./shared/components/navigation/MainNavigation";
-import { AuthContext } from "./shared/context/auth-context";
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
+import MainNavigation from './shared/components/navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
+import { useAuth } from './shared/hooks/auth-hook';
 
 const App = () => {
-  const [token, setToken] = useState(null);
-  const [userId, setUserId] = useState(false);
-
-  const login = useCallback((uid, token) => {
-    setToken(token);
-    setUserId(uid);
-  }, []);
-
-  const logout = useCallback(() => {
-    setToken(null);
-    setUserId(null);
-  }, []);
+  const { token, login, logout, userId} = useAuth();
 
   let routes;
 
@@ -67,12 +57,13 @@ const App = () => {
 
   return (
     <AuthContext.Provider
-      value={{ 
-        isLoggedIn: !!token, 
+      value={{
+        isLoggedIn: !!token,
         token: token,
         userId: userId,
-        login: login, 
-        logout: logout }}
+        login: login,
+        logout: logout
+      }}
     >
       <Router>
         <MainNavigation />
