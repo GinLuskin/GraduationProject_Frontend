f71268e7a984946b1638df40de4e398db2edda65
Regina Luskin <reginash12@gmail.com>
2023-01-22 21:29:44 +0200

    issue fixed

--- Back/util/location.js
+++ Back/util/location.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const HttpError = require('../models/http-error');
 
-const API_KEY = process.env.GOOGLE_API_KEY;
+const API_KEY = 'AIzaSyCx7fGdUsUEBoXGtWgpgWfGmk-rg0FZrq4';
 
 async function getCoordsForAddress(address) {
 
